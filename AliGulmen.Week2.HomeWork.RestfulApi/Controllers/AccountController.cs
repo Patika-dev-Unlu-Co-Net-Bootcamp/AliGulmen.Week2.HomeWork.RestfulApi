ae1b22046b01080230a843f1e87f2fec40f5195c
[Ali Gulmen] <aligulmen91@gmail.com>
2022-01-21 18:53:13 +0300

    AccountController for log-in check created

---
+++ AliGulmen.Week2.HomeWork.RestfulApi/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using AliGulmen.Week2.HomeWork.RestfulApi.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AliGulmen.Week2.HomeWork.RestfulApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+
+        /************************************* DELETE *********************************************/
+
+        //DELETE api/locations/id
+        [HttpGet]
+        [CustomPermission("admin")]
+
+
+        public IActionResult IsLoggedIn(string username, string password)
+        {
+            var pass = "1234";
+
+            var permissions = typeof(AccountController)
+                .GetMethod("IsLoggedIn")
+                .GetCustomAttributes(typeof(CustomPermissionAttribute), true)
+                .Cast<CustomPermissionAttribute>()
+                .Select(x => x.Permission).ToArray();
+
+            if(permissions.Any(p => p == username) && password == pass)
+            return Ok();
+            return Unauthorized();
+        }
+
+
+
+    }
+}
